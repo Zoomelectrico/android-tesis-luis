946a933372b67a3d3c228fccc5e6b67b54dadc46
Jose Roberto Quevedo <jose.quevedo2011@gmail.com>
2018-09-26 17:30:38 -0400

    viendo la luz al final del tunel

--- app/src/main/java/com/example/zoomelectrico/tesis_ucab/WorkerActivity.java
+++ app/src/main/java/com/example/zoomelectrico/tesis_ucab/WorkerActivity.java
@@ -7,6 +7,11 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.Result;
@@ -48,10 +53,44 @@ public class WorkerActivity extends AppCompatActivity implements ZXingScannerVie
     }
 
     @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mScannerView.stopCamera();
+        mScannerView = null;
+
+    }
+
+    @Override
     public void handleResult(Result rawResult) {
-        Log.e("Result", rawResult.getText());
-        Log.e("Result", rawResult.getBarcodeFormat().toString());
-        mScannerView.resumeCameraPreview(this);
+        String string = rawResult.getText();
+        if(string != null ){
+            Log.e("Results", string);
+            mScannerView.stopCamera();
+            String[] datos = string.split(";");
+            setContentView(R.layout.activity_worker);
+            configUI(datos);
+        }
+        // mScannerView.resumeCameraPreview(this);
+    }
+
+    private void configUI(String[] datos) {
+        String[] statuses = new String[] {
+                "Entrando al Sistema",
+                "Entregado al Destinatario en Oficina",
+                "Recibido en Oficina de Transito",
+                "Recibido en Oficina de Destino",
+                "Enviado en Transporte",
+                "Entregado al Destinatario en Direccion",
+                "Usuario no Encontrado",
+                "Transporte de Entrega"
+        };
+        Log.e("datos",datos.toString());
+        ((TextView) findViewById(R.id.txtIdWorker)).setText(datos[0]);
+        ((TextView) findViewById(R.id.txtStatusWorker)).setText(statuses[1]);
+        Spinner spinner = ((Spinner) findViewById(R.id.spinnerStatus));
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, statuses);
+        spinner.setAdapter(adapter);
+
     }
 
     private void permissionHandler() {
